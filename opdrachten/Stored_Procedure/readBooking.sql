d658e794831eeccccbbe81dcdcef95f3586946d2
333736 <90184972+NijsvanDuin@users.noreply.github.com>
2023-01-11 11:55:29 +0100

    Verandering Read Proses

--- opdrachten/Stored_Procedure/readBooking.sql
+++ opdrachten/Stored_Procedure/readBooking.sql
@@ -1,26 +1,19 @@
 CREATE PROCEDURE `readBooking` ()
 BEGIN
-
-Select   pas.id
-		,pas.Voornaam
-		,pas.Achternaam
-        ,con.id
-        ,con.Adres
-        ,con.Email
-        ,vlu.Bestemming
-        ,vlu.Vertrekdatum
-        ,vlu.Vertrektijd
-        ,vlu.Vluchtnummer
-        ,vlu.id
-
-From instapkaart 			as ins
-inner join passagier 		as pas
-on ins.PassagierId = pas.id
-
-inner join contact			as con
-on con.PassagierId = pas.id
-
-inner join vlucht			as vlu
-on ins.VluchtId = vlu.id;
-
-END;
+SELECT pas.Voornaam,
+    pas.Achternaam,
+    con.Adres,
+    con.Email,
+    vlu.Bestemming,
+    vlu.Vertrekdatum,
+    vlu.Vertrektijd,
+    vlu.Vluchtnummer,
+    ins.id AS insId,
+    vlu.id AS vluId,
+    pas.id AS pasId,
+    con.id AS conId
+    FROM Instapkaart AS ins 
+    INNER JOIN Passagier AS pas ON ins.PassagierId = pas.id
+    INNER JOIN Contact AS con ON con.PassagierId = pas.id 
+    INNER JOIN Vlucht AS vlu ON ins.VluchtId = vlu.id;
+END
